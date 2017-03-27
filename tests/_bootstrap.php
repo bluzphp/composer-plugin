ac058de764f0c106bbd68dfd02b9aed00f228c4a
Anton Shevchuk <AntonShevchuk@gmail.com>
2017-03-27 16:02:21 +0300

    Fixed boostrap tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -5,6 +5,11 @@ define('DEBUG', true);
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 
+// Paths
+define('PATH_ROOT', realpath(dirname(__FILE__). '/../'));
+define('PATH_APPLICATION', PATH_ROOT . '/tests');
+define('PATH_VENDOR', PATH_ROOT . '/vendor');
+
 // init autoloader
 $loader = require PATH_ROOT . '/vendor/autoload.php';
 $loader->addPsr4('Bluz\\Tests\\', __DIR__ .'/src');
